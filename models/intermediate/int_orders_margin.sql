dab4c12a7ff60b1fe31e720a71a83849506033e3
Vania Benavente <vanibt15@gmail.com>
2025-09-16 12:13:46 +0000

    orders margin

--- models/intermediate/int_orders_margin.sql
+++ models/intermediate/int_orders_margin.sql
@@ -1,11 +1,10 @@
-SELECT s.orders_id,
-    s.date_date,
-    ROUND(SUM(s.revenue),2) AS revenue,
-    SUM(s.quantity) AS quantity,
-    SUM((s.quantity * p.purchase_price)) AS purchase_cost,
-    ROUND(SUM(s.revenue - (s.quantity * p.purchase_price)), 2) AS margin
-FROM {{ ref ('stg_raw__sales')}} AS s
-INNER JOIN {{ ref ('stg_raw__product')}} AS p
-ON s.products_id = p.products_id
-GROUP BY s.orders_id, date_date
-ORDER BY s.orders_id DESC
+ SELECT
+     orders_id,
+     date_date,
+     ROUND(SUM(revenue),2) as revenue,
+     ROUND(SUM(quantity),2) as quantity,
+     ROUND(SUM(purchase_cost),2) as purchase_cost,
+     ROUND(SUM(margin),2) as margin
+ FROM {{ ref("int_sales_margin") }}
+ GROUP BY orders_id,date_date
+ ORDER BY orders_id DESC
